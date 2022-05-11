c65606b817f9cbb3c6d21145d0465a021242a40a
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-05-11 19:54:42 +0200

    optimisation lasers (+10fps)

--- src/babylon_start/scene.js
+++ src/babylon_start/scene.js
@@ -26,7 +26,7 @@ class Scene {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     tanksAIReady = false;
-    this.engine = new BABYLON.Engine(canvas, true, null, true);
+    this.engine = new BABYLON.Engine(canvas, true, null, false);
 
     engine = this.engine;
     engine.enableOfflineSupport = false;
